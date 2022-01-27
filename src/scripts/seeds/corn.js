0d64aac915343ca13f57c96b95d2f5250202cf9a
Rick Kersmarki <rkersmarki@gmail.com>
2022-01-27 12:09:09 -0600

    plant seeds added

---
+++ src/scripts/seeds/corn.js
@@ -0,0 +1,14 @@
+export const createCorn = () => {
+    return [{
+        id: 1,
+        type: "Corn",
+        height: 180,
+        output: 6
+    },
+    {
+        id: 2,
+        type: "Corn",
+        height: 180,
+        output: 6
+    }]
+}
